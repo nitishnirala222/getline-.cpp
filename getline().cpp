fb37eb4d225d90611c8201e2572d4af10d722161
Nitish Nirala <42496192+nitishnirala222@users.noreply.github.com>
2018-12-23 11:35:35 +0530

    Add files via upload

---
+++ getline().cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string str;
+cout<<"Please enter your name: ";
+getline(cin,str);
+cout<<"Hello "<<str<<" well come to Nirala Github profile !";
+}
